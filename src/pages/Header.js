fe5323b50484c95d49f4055b773473b5ad361dac
TungSLTR <thanhtung17112003@gmail.com>
2023-09-08 14:05:33 +0700

    ...

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -376,9 +376,9 @@ export default function Header() {
           </div>
         </Link>
         {showBackToTop && (
-          // <Link onClick={scrollToTop} id="back-to-top" href="#" passHref>
-            <img onClick={scrollToTop} id="back-to-top" width={30} src="/img/home/up-arrow.png"></img>
-        // </Link>
+          <button onClick={scrollToTop} id="back-to-top">
+            <img width={30} src="/img/home/up-arrow.png"></img>
+        </button>
         )}
       </div>
       <section className="shopify-section spaced-header">
