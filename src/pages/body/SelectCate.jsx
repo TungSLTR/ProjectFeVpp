a6c93a23006d890bfdb8d264392d8e6226779e3c
TungSLTR <thanhtung17112003@gmail.com>
2023-11-05 19:37:19 +0700

    Update SelectCate.jsx

--- src/pages/body/SelectCate.jsx
+++ src/pages/body/SelectCate.jsx
@@ -7,72 +7,72 @@ const SelectCate = () => {
       id: 1,
       name: "Bút mực",
       imageSrc: "img/home/pens_collection.webp",
-      link: "pen",
+      link: "giay-in-van-phong",
     },
     {
       id: 2,
       name: "Bút chì",
       imageSrc: "img/home/pencil_collection.webp",
-      link: "pencil",
+      link: "giay-in-van-phong",
     },
     {
       id: 3,
       name: "Giấy Planner",
       imageSrc: "img/home/planner.webp",
-      link: "planner",
+      link: "giay-in-van-phong",
     },
     {
       id: 4,
       name: "Notebook",
       imageSrc: "img/home/notebook.webp",
-      link: "notebook",
+      link: "giay-in-van-phong",
     },
     {
       id: 5,
       name: "Notepad",
       imageSrc: "img/home/notepad.webp",
-      link: "notepad",
+      link: "giay-in-van-phong",
     },
     {
       id: 6,
       name: "Nhật ký",
       imageSrc: "img/home/journals.webp",
-      link: "nhatky",
+      link: "giay-in-van-phong",
     },
     {
       id: 7,
       name: "Bút highlight",
       imageSrc: "img/home/highlighters.webp",
-      link: "highlight",
-    },{
+      link: "giay-in-van-phong",
+    }, {
       id: 8,
       name: "Gôm",
       imageSrc: "img/home/eraser.webp",
-      link: "gom",
+      link: "giay-in-van-phong",
     },
     {
       id: 9,
       name: "Memo Blocks",
       imageSrc: "img/home/memo.webp",
-      link: "memo",
+      link: "giay-in-van-phong",
     },
     {
       id: 10,
       name: "Sharpener",
       imageSrc: "img/home/sharpener.webp",
-      link: "sharpener",
+      link: "giay-in-van-phong",
     },
     {
       id: 11,
       name: "To-do Lists",
       imageSrc: "img/home/todo.webp",
-      link: "todo",
+      link: "giay-in-van-phong",
     },
     {
       id: 12,
       name: "Sticky Notes",
       imageSrc: "img/home/sticky.webp",
-      link: "sticky",
+      link: "giay-in-van-phong",
     },
   ];
 
@@ -82,12 +82,12 @@ const SelectCate = () => {
         <div className="new-flex">
           {collectItems.map((item) => (
             <div className="flexitem" key={item.id}>
-              <Link className="linkitem" href={`/product/${item.link}`}>
+              <Link className="linkitem" href={`/${item.link}`}>
                 <div className="img-circle">
                   <div className="collection-img">
                     <img className="imgcollect" src={item.imageSrc} loading="lazy" />
                   </div>
-                 
+
                 </div>
 
                 <span className="collection-title">{item.name}</span>
